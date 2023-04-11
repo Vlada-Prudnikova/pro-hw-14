eedcf5e31af4623dbd1523a10e21536f484f6546
git-pages <vlada.prudnikova.2004@gmail.com>
2023-04-11 13:18:36 -0400

    recurcia commit

--- index2.js
+++ index2.js
@@ -1,9 +1,5 @@
 function pow(number, degree) {
-  let result = number;
-  for (let i = 1; i < degree; i++) {
-    result *= number;
-  }
-  return result;
+  return degree == 1 ? number : number * pow(number, degree - 1);
 }
 
-console.log(pow(7, 2));
+console.log(pow(8, 7));
